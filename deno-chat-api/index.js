e1d740cde3f78473d2c5836d9f46e3affcd185e8
Josh Barnes <joshbarnes@gmail.com>
2022-01-31 21:28:20 +0000

    Add oak

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,6 +1,9 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World 2!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World 3!";
 });
+
+await app.listen({ port: 8000 });
